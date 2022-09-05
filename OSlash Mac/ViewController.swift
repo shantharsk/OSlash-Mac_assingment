a804f4b1767b28d30bd3fb60329b688d5a3b4ac9
shantharsk <shantharsk@gmail.com>
2022-09-05 17:08:29 +0530

    initial commit

---
+++ OSlash Mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OSlash Mac
+//
+//  Created by Shantha on 9/5/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
